e5f1960476e75121f2895f1e4e548772c9a82b9c
Mykhailo <davudenko.mukhailo@gmail.com>
2024-03-13 15:05:28 +0200

    added second lab

---
+++ Lab_2.1/FloatRange.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Lab_1._1;
+
+public class FloatRange
+{
+    private double First { get; set; }
+    private double Second { get; set; }
+
+    public FloatRange(double first, double second)
+    {
+        if (first < second)
+        {
+            First = first;
+            Second = second;
+        }
+        else
+        {
+            Console.WriteLine("Error, first value is not less than second value");
+            // Setting default values if the condition is not met
+            First = 0;
+            Second = 0;
+        }
+    }
+
+    public FloatRange()
+    {
+        // Default constructor without arguments
+        First = 0;
+        Second = 0;
+    }
+
+    // Copy constructor
+    public FloatRange(FloatRange other)
+    {
+        First = other.First;
+        Second = other.Second;
+    }
+
+    public void Read()
+    {
+        double firstValue;
+        double secondValue;
+        do
+        {
+            Console.Write("Enter first value: ");
+            firstValue = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second value: ");
+            secondValue = Convert.ToDouble(Console.ReadLine());
+        }
+        while (firstValue >= secondValue);
+
+        First = firstValue;
+        Second = secondValue;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"First value: {First}");
+        Console.WriteLine($"Second value: {Second}");
+    }
+
+    public bool RangeCheck(double x)
+    {
+        return (First <= x && x <= Second);
+    }
+
+    // Friend functions for input/output
+    public static void Read(FloatRange floatRange)
+    {
+        floatRange.Read();
+    }
+
+    public static void Display(FloatRange floatRange)
+    {
+        floatRange.Display();
+    }
+
+    // Operator overloading for prefix increment
+    public static FloatRange operator ++(FloatRange floatRange)
+    {
+        floatRange.First++;
+        return floatRange;
+    }
+    // Operator overloading for prefix decrement
+    public static FloatRange operator --(FloatRange floatRange)
+    {
+        floatRange.First--;
+        return floatRange;
+    }
+
+    // Conversion to string
+    public static implicit operator string(FloatRange floatRange)
+    {
+        return $"First value: {floatRange.First}, Second value: {floatRange.Second}";
+    }
+}
+
+
---
+++ Lab_2.1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Lab_1._1;
+
+public class Program
+{
+    static FloatRange MakeFloatRange(double first, double second)
+    {
+        return new FloatRange(first, second);
+    }
+
+    static void Main(string[] args)
+    {
+        FloatRange floatRange = new();
+        FloatRange.Read(floatRange);
+        FloatRange.Display(floatRange);
+        double x;
+        Console.Write("Enter number: ");
+        x = Convert.ToDouble(Console.ReadLine());
+        if (floatRange.RangeCheck(x))
+        {
+            Console.WriteLine("Number is in range");
+        }
+        else
+        {
+            Console.WriteLine("Number is not in range");
+        }
+
+        double firstValue;
+        double secondValue;
+
+        Console.Write("Enter first value: ");
+        firstValue = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second value: ");
+        secondValue = Convert.ToDouble(Console.ReadLine());
+
+        floatRange = MakeFloatRange(firstValue, secondValue);
+        FloatRange.Display(floatRange);
+
+        // Demonstrating prefix increment
+        FloatRange incrementedFloatRange = ++floatRange;
+        Console.WriteLine("After prefix increment:");
+        Console.WriteLine(incrementedFloatRange);
+
+        // Demonstrating postfix increment
+        incrementedFloatRange = floatRange++;
+        Console.WriteLine("After postfix increment:");
+        Console.WriteLine(incrementedFloatRange);
+
+        // Demonstrating prefix decrement
+        FloatRange decrementedFloatRange = --floatRange;
+        Console.WriteLine("After prefix decrement:");
+        Console.WriteLine(decrementedFloatRange);
+
+        // Demonstrating postfix decrement
+        decrementedFloatRange = floatRange--;
+        Console.WriteLine("After postfix decrement:");
+        Console.WriteLine(decrementedFloatRange);
+    }
+}
